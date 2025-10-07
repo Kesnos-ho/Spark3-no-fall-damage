5cbba4fe616d41c1d5afa60f352b6d392d355a45
Kesnos-ho <76844828+Kesnos-ho@users.noreply.github.com>
2025-10-07 23:03:40 +0200

    Add files via upload

---
+++ Spark3_nofalldamage/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using MelonLoader;
+using Spark3_nofalldamage; // The namespace of your mod class
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+// ...
+[assembly: MelonInfo(typeof(NoFallDamage), "No Fall Damage", "1.0", "Kesnos")]
+[assembly: MelonGame("Feperd Games", "Spark the Electric Jester 3")]
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("Spark3_nofalldamage")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Atlas Playbook v0.4.1")]
+[assembly: AssemblyProduct("Spark3_nofalldamage")]
+[assembly: AssemblyCopyright("Copyright © Atlas Playbook v0.4.1 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("4f5d6a48-15fd-48a9-be2d-805ab641a393")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
